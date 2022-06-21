33f428ff2f89bca8a446498721cca5004646bd1a
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-06-21 18:42:49 +0100

    🎨 Adding exa

--- rc.d/zshrc.d/aliases.zsh
+++ rc.d/zshrc.d/aliases.zsh
@@ -2,3 +2,10 @@ alias dotfiles="$HOME/.dotfiles/dotfiles"
 
 # Use bat instead of cat if available.
 hash bat 2>/dev/null && alias cat="bat"
+
+# exa Aliases
+if hash exa 2>/dev/null; then
+    alias tree="exa --tree --level=4 --long"
+    alias ls="exa --long --header --git"
+    alias ll="exa --all --all --long --header --git"
+fi
