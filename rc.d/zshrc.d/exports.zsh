297cc1a9a8ccad38baf4d02b856a87cc8ca643ef
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-07-24 16:08:30 +0100

    🎨 Added nvm config if exists.

--- rc.d/zshrc.d/exports.zsh
+++ rc.d/zshrc.d/exports.zsh
@@ -27,3 +27,10 @@ fi  && source ~/.fzf.zsh
 
 # Set the bat(cat replacement) theme
 export BAT_THEME="Solarized (dark)"
+
+# If nvm is installed, add
+if [ -f "$HOME"/.nvm/nvm.sh ]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
